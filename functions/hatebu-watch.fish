41e9b20ffaa8d89537832e00dbb6ba895b9b3563
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-07-23 23:41:46 +0900

    Implement notification

--- functions/hatebu-watch.fish
+++ functions/hatebu-watch.fish
@@ -4,6 +4,18 @@ function hatebu-watch -d "Watch the Hatena Bookmark count of specific post"
   while true
     eval "command curl -s 'http://api.b.st-hatena.com/entry.count?url=$url'" | read -z count
     echo -e $count"\r\c"
+
+    if [ -z "$prev_count" ]
+      set prev_count $count
+    end
+ 
+    if [ $count -gt $prev_count ]
+      if type -q terminal-notifier
+        terminal-notifier -message "$count" -title "$url" -sender "hatebu-watch" 
+      end
+    end
+
+    set -l prev_count $count
     sleep 60
   end
 end
